4d317f10fe30bcb993eacab71351b560c76a1448
Rob Cameron <cannikinn@gmail.com>
2012-04-10 21:30:24 -0700

    Removed sample.rb

--- sample.rb
+++
@@ -1,6 +0,0 @@
-output = {}
-permissions = Role.first.permissions.includes(:permission_type)
-permissions.each do |per|
-  output.deep_merge!({ per.permission_type.object_type.to_sym => { per.permission_type.klass.to_sym => { per.permission_type.key.to_sym => per.enabled } } })
-end
-ap output
